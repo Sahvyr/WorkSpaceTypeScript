1494461dfb3324a576aa9631a2343bbb3fe4ad98
Thiago Rodrigues <thiago.rodrigues365701@gmail.com>
2025-07-27 18:35:49 -0300

    feat: criar pasta deverDeCasa e fazer exercício da aula 02 em TypeScript
    - Criada pasta deverDeCasa para organizar os arquivos.
    - Implementado exercício da aula 02 com enumeração de status e função para mensagens.

---
+++ deverDeCasa/deverAula02/src/index.ts
@@ -0,0 +1,26 @@
+enum Status {
+  emAndamento,
+  emAtraso,
+  criado,
+  cancelado,
+  finalizado,
+}
+
+function pegarStatus(status: Status): string {
+  switch (status) {
+    case Status.emAndamento:
+    case Status.emAtraso:
+    case Status.criado:
+      return "Estamos avaliando o seu pedido";
+    case Status.cancelado:
+      return "Seu pedido foi cancelado. Ligue para XXXXX";
+
+    case Status.finalizado:
+      return "Seu pedido foi finalizado na data XX/XXX";
+
+    default:
+      return "Status desconhecido";
+  }
+}
+
+console.log(pegarStatus(Status.cancelado));
